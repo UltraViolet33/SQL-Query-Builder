3c3d8f7c54e5c023fe5abd88af3ba7152afc6468
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-01 16:01:49 +0100

    add select method

--- src/Finder.php
+++ src/Finder.php
@@ -4,8 +4,23 @@ namespace App;
 
 class Finder
 {
-    public function __construct()
+
+    public static $sql = "";
+    public static $instance = NULL;
+    public static $prefix = "";
+    public static $where = array();
+    public static $control = ["", ""];
+
+    public static function select($tableName, $columns = NULL)
     {
-        echo "ok";
+        self::$instance = new Finder();
+
+        if ($columns) {
+            self::$prefix = 'SELECT ' . $columns . ' FROM ' . $tableName;
+        } else {
+            self::$prefix = 'SELECT * FROM ' . $tableName;
+        }
+
+        return self::$instance;
     }
 }
