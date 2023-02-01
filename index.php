05c74921f9b0cea872315894be2c17dcd7612e9c
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-01 15:56:53 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+use App\Finder;
+
+$finder = new Finder();
---
+++ src/Finder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Finder
+{
+    public function __construct()
+    {
+        echo "ok";
+    }
+}
